c99897caccfd1d3c9033285bbfe7efaf5477836b
Dustin Schie <dustin@schie.io>
2020-10-01 00:24:03 -0400

    :pencil: (example/src/App) adding an overly convoluted example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { useMyHook } from 'use-maintained-ref'
+import { useMaintainedRef } from 'use-maintained-ref';
+
+const RandoButton = ({ onSomeEvent, text }) => {
+  const [value, setValue] = useState(false);
+  const onSomeEventRef = useMaintainedRef(onSomeEvent);
+
+  const toggle = useCallback(async () => {
+    setValue(!value);
+  }, [text, value]);
+
+  // will call the `onSomeEvent` function prop, but won't be triggered by it
+  useEffect(() => {
+    onSomeEventRef.current({ event: 'toggleChanged', value });
+  }, [value]);
+
+  // will call the `onSomeEvent` function prop, but won't be triggered by it
+  useEffect(() => {
+    onSomeEventRef.current({ event: 'textChanged', value: text });
+  }, [text]);
+
+  return (
+    <button onClick={toggle} on>
+      {text} - {value.toString()}
+    </button>
+  );
+};
 
 const App = () => {
-  const example = useMyHook()
+  const [text, setText] = useState('test');
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setText(new Date().toLocaleTimeString());
+    }, [1000]);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  const [eventData, setEventData] = useState(null);
+
   return (
     <div>
-      {example}
+      <RandoButton text={text} onSomeEvent={(eventData) => setEventData(eventData)} />
+      <pre>{JSON.stringify(eventData, null, 2)}</pre>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
